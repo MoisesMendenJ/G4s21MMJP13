41a70b5d5161ae17f36bcdc1e25c40b93c082075
52561 <52561@192.168.1.72>
2022-05-23 11:41:48 -0500

    Mendez Jaramillo Moises 4s21

---
+++ src/main/java/com/mycompany/g4s21mmjp13/G4s21MMJP13.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.g4s21mmjp13;
+
+/**
+ *
+ * @author 52561
+ */
+public class G4s21MMJP13 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new JFPrincipal().setVisible(true);
+    }
+}
